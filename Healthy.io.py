495db2eb1b7e84610a457fbfaa5d5e8899752e5f
efihadar <59658157+efihadar@users.noreply.github.com>
2021-02-15 21:12:17 +0200

    Challenge-S3
    Home assignment: DevOps position

---
+++ Healthy.io.py
@@ -0,0 +1,20 @@
+import datetime
+import os
+
+for root, dirs, files in os.walk("e"):
+    for filename in files:
+        filename1 = filename.replace(".gz", "")
+        date_time_str = filename1
+        date_time_obj = datetime.datetime.strptime(date_time_str, '%d-%m-%y')
+        now = datetime.datetime.now()
+        current_time = now.strftime("%H-%M-%S")
+
+        print("Old file name: " + root + "\\" + filename)
+        print("New file name: " + root + "\\" + str(date_time_obj.date()) + "T" + current_time + ".gz")
+
+        ##Actual name change
+        #os.system("mv " + root + "\\" + filename + root + "\\" + str(date_time_obj.date()) + "T" + current_time)
+
+
+
+    #aws s3 --recursive mv s3://efibackups/<folder_name_from> s3://efibackups/<folder_name_to>
